4902cc92f16a322e3c985403a52ae18380f46784
BoevMaksim <maksim.schetina@gmail.com>
2020-08-10 16:24:49 +0300

    add text button, field and transaction detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+import 'package:intl/intl.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -15,14 +18,112 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      amount: 30.45,
+      date: DateTime.now(),
+      title: 'New Shoes',
+    ),
+    Transaction(
+      id: 't2',
+      amount: 100.00,
+      date: DateTime.now(),
+      title: 'New iPhone',
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Budget Calculator'),
       ),
-      body: Center(
-        child: Text('Text'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: Text('Chart!'),
+              ),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    textColor: Colors.purple,
+                    padding: EdgeInsets.only(top: 10.0, right: 3.0),
+                    child: Text('Add Transaction'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transaction.map((e) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${e.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          e.title.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        Text(
+                          DateFormat.yMMMMd().add_Hm().format(e.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
